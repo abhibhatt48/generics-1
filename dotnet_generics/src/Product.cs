519870e602393255b3ab79edd6b31a0c46b3fc85
abhibhatt48 <47849440+abhibhatt48@users.noreply.github.com>
2022-03-14 13:12:58 +0530

    4 features are done 2 more to go

---
+++ dotnet_generics/src/Product.cs
@@ -0,0 +1,14 @@
+namespace src
+{
+    public class Product
+    {
+        public string pname { get; set; }
+        public string price { get; set; }
+        public int quantity { get; set; }
+        public string type { get; set; }
+
+       
+
+
+    }
+}
